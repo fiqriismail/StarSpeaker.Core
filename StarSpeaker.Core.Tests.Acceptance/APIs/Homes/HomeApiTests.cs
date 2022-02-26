c07cfb10cb58a9d9de4dd2b179f612f36447247b
Fiqri Ismail <fiqri.ismail@gmail.com>
2022-02-26 22:03:36 +0530

    INFRA: Initialized the acceptance test project

---
+++ StarSpeaker.Core.Tests.Acceptance/APIs/Homes/HomeApiTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using StarSpeaker.Core.Tests.Acceptance.Brokers;
+using Xunit;
+
+namespace StarSpeaker.Core.Tests.Acceptance.APIs.Homes
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class HomeApiTests
+    {
+        private readonly StarSpeakerApiBroker _starSpeakerApiBroker;
+
+        public HomeApiTests(StarSpeakerApiBroker starSpeakerApiBroker) =>
+            _starSpeakerApiBroker = starSpeakerApiBroker;
+
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            // given
+            string expectedMessage =
+                "Talk is cheap. Show me the code. ~ Linus Torvalds";
+
+            // when 
+            string actualMessage = await _starSpeakerApiBroker.GetHomeMessageAsync();
+
+            // then
+            actualMessage.Should().BeEquivalentTo(expectedMessage);
+        }
+    }
+}
---
+++ StarSpeaker.Core.Tests.Acceptance/Brokers/ApiTestCollection.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------------------------------
+// Copyright (C) Fiqri Ismail 
+// Created just for fun for learning purposes of "The Standard" 
+// The Standard: https://github.com/hassanhabib/The-Standard
+// -----------------------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace StarSpeaker.Core.Tests.Acceptance.Brokers
+{
+    [CollectionDefinition(nameof(ApiTestCollection))]
+    public class ApiTestCollection: ICollectionFixture<StarSpeakerApiBroker>
+    {
+    }
+}
